dd3e96d54894dd563be0660436a614bdfdf51278
Izolda Arshakyan <izolda.arshakyan@volo.global>
2024-06-04 12:40:15 +0400

    admin role change by url fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { Box, Button, Typography } from '@mui/material'
-import { selectIsPending, selectProfileData } from 'store/selectors'
+import { selectIsLoggedIn, selectIsPending, selectProfileData } from 'store/selectors'
 import { useLogout } from 'hooks/auth/useLogout'
 import { useAppSelector } from 'hooks/useAppSelector'
 import { PRIVATE_PAGES, PUBLIC_PAGES } from 'constants/pages'
@@ -10,6 +10,7 @@ import AppNavLink from 'components/ui/appNavLink'
 const HomePage: FC = () => {
   const { email, role } = useAppSelector(selectProfileData)
   const isPending = useAppSelector(selectIsPending)
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
 
   const handleLogoutClick = useLogout()
 
@@ -19,7 +20,7 @@ const HomePage: FC = () => {
         Welcome
       </Typography>
       <Typography paragraph>{email}</Typography>
-      {role === ROLES.admin && (
+      {role === ROLES.admin && isLoggedIn && (
         <AppNavLink primary to={PRIVATE_PAGES.invitation}>
           Invite
         </AppNavLink>
